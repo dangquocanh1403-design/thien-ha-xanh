d26adbf2540b997719d75ad302051feaf1dd3aed
dangquocanh1403-design <dangquocanh1403@gmail.com>
2026-08-25 15:48:45 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 "use strict";
-const CACHE="thien-ha-xanh-v26";
+const CACHE="thien-ha-xanh-v29";
 self.addEventListener("install",()=>self.skipWaiting());
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",event=>event.respondWith(fetch(event.request).catch(()=>caches.match(event.request))));
